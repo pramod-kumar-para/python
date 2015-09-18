b96ab56b96733bf52d91ae4c1e19205fc8a4b37a
pramod-kumar-para <pramodkumarpara95@gmail.com>
2015-09-18 11:42:12 +0530

    python basics

---
+++ basics/del_statement.py
@@ -0,0 +1,12 @@
+# We can remove items from the list by indexing or slicing using delete statement
+
+
+l=[1,2,3,4,5,6,7,8,9]
+
+# deleting items by indexing
+del l[0]
+print(l)
+
+# deleting items by slicing
+del l[3:]
+print(l)
