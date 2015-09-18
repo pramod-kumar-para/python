acb014c6738878004450c31e715ff819c49f6078
pramod-kumar-para <pramodkumarpara95@gmail.com>
2015-09-18 11:24:41 +0530

    python basics

---
+++ basics/functions/unpacking_arguments2.py
@@ -0,0 +1,12 @@
+__author__ = 'Pramodh'
+
+
+l=[1,10,3]
+
+x=list(range(*l))
+# arguments of range function are start,stop,step
+print(x)
+
+# The above code is similar to
+
+print(list(range(1,10,3)))
